ee5de2f743eb8a752438708f819a0b7c2873ac27
Mitsu Uemura <mitsuhide.uemura@gmail.com>
2024-10-19 23:07:24 +1100

    しりとりの指示とともに AI が返した答えをオウム返しに渡して、AI の一人しりとりにした。

--- shiritori-AI.py
+++ shiritori-AI.py
@@ -9,15 +9,17 @@ genai.configure(api_key=api_key)
 mecab = MeCab.Tagger()
 
 
-def is_noun(w):
-    node = mecab.parseToNode(w)
-    while node:
-        if node.feature.split(",")[0] == '名詞':
-            return True
-        node = node.next
-    return False
-
+instruction = """私は日本語を勉強しています。あなたには、しりとりの相手役をお願いします。しりとりの答えだけを、
+単語ひとつで答えて下さい。ひらがなだけで答えて下さい。一般名詞で答えて下さい。"""
+word = "しりとり"
 
 model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Hello world!")
-print(response.text)
+
+while True:
+    prompt = instruction + word
+    print(prompt)
+    response = model.generate_content(prompt)
+    print("===")
+    word = response.text
+    print(word)
+    print("===")
