77ebfc31d030e1eefecf3a6a471194482b256440
Mitsu Uemura <mitsuhide.uemura@gmail.com>
2024-10-19 23:08:55 +1100

    しりとりクラスを定義した。

--- shiritori-AI.py
+++ shiritori-AI.py
@@ -9,6 +9,19 @@ genai.configure(api_key=api_key)
 mecab = MeCab.Tagger()
 
 
+class Shiritori(List):
+    def __init__(self, initial_word):
+        self.initial_word = initial_word
+
+    def is_noun(self, w):
+        node = mecab.parseToNode(w)
+        while node:
+            if node.feature.split(",")[0] == '名詞':
+                return True
+            node = node.next
+        return False
+
+
 instruction = """私は日本語を勉強しています。あなたには、しりとりの相手役をお願いします。しりとりの答えだけを、
 単語ひとつで答えて下さい。ひらがなだけで答えて下さい。一般名詞で答えて下さい。"""
 word = "しりとり"
