e7bcc27eaf88828c929c6e13aac5ef5c60742c26
Mitsu Uemura <mitsuhide.uemura@gmail.com>
2024-10-26 21:20:52 +1100

    Check word length.

--- shiritori-AI.py
+++ shiritori-AI.py
@@ -18,18 +18,28 @@ class NotNounException(Exception):
         return "not a noun"
 
 
+class LessThanTwoLettersException(Exception):
+    def __str__(self):
+        return "The word consists of less than two letters"
+
+
 class Shiritori(dict):
     def __init__(self, initial_word):
         self[initial_word] = ""
 
     def add(self, w):
-        if self._is_noun(w):
+        if self._is_legit(w):
             self[w] = ""
-        else:
-            raise NotNounException()
 
         if args.debug:
             print(f"DEBUG: {self=}")
+
+    def _is_legit(self, w):
+        if len(w) < 2:
+            raise LessThanTwoLettersException()
+        if not self._is_noun(w):
+            raise NotNounException()
+        return True
 
     def _is_noun(self, w):
         node = mecab.parseToNode(w)
@@ -57,6 +67,8 @@ while True:
         print(f"DEBUG: {word=}")
     try:
         shiritori.add(word)
+    except LessThanTwoLettersException:
+        prompt = "二文字以上の単語だけで答えてください。"
     except NotNounException:
         prompt = "答えが一般名詞ではありません。"
     else:
