c4355f668900db71089ed783ae090856f4b79dae
Mitsu Uemura <mitsuhide.uemura@gmail.com>
2024-10-26 19:00:15 +1100

    Inherit not list but dictionary then add words to it.

--- shiritori-AI.py
+++ shiritori-AI.py
@@ -1,6 +1,5 @@
 import google.generativeai as genai
 import MeCab
-import os
 
 with open("./Google-AI.key") as key_file:
     api_key = key_file.read().rstrip()
@@ -9,14 +8,19 @@ genai.configure(api_key=api_key)
 mecab = MeCab.Tagger()
 
 
-class Shiritori(list):
+class Shiritori(dict):
     def __init__(self, initial_word):
-        self.initial_word = initial_word
+        self[initial_word] = ""
 
-    def is_noun(self, w):
+    def add(self, w):
+        if self._is_noun(w):
+            self[w] = ""
+        print(self)
+
+    def _is_noun(self, w):
         node = mecab.parseToNode(w)
         while node:
-            if node.feature.split(",")[0] == '名詞':
+            if node.feature.split(",")[0] == "名詞i":
                 return True
             node = node.next
         return False
@@ -27,12 +31,14 @@ instruction = """私は日本語を勉強しています。あなたには、し
 word = "しりとり"
 
 model = genai.GenerativeModel("gemini-1.5-flash")
+shiritori = Shiritori(word)
 
 while True:
     prompt = instruction + word
     print(prompt)
     response = model.generate_content(prompt)
     print("===")
-    word = response.text
-    print(word)
+    word = response.text.rstrip()
+    print("'" + word + "'")
+    shiritori.add(word)
     print("===")
